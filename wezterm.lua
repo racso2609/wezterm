afc40b4c10085a5e4a453067deaf405559c64afe
Oscar Lugo <oscaremiliolugo@gmail.com>
2022-04-10 02:38:50 +0000

    wezterm theme

--- wezterm.lua
+++ wezterm.lua
@@ -3,14 +3,10 @@ local wezterm = require 'wezterm';
 return {
   enable_tab_bar = false,
   window_background_opacity = 0.8,
-  -- color_scheme = "Afterglow",
-  -- color_scheme = "BlueBerryPie",
-  -- color_scheme = "Batman",
   color_scheme = "Calamity",
   font = wezterm.font("CaskaydiaCove Nerd Font"),
   enable_scroll_bar = true,
   keys = {
-    -- {key="A", mods="CTRL", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
     {key="n", mods="CTRL", action="SpawnWindow"},
     {key="l", mods="CTRL|SHIFT", action=wezterm.action{ClearScrollback="ScrollbackOnly"}},
   }
