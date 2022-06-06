ca3314616e69c55bd0cee61df2f0dd6d65e6751b
Oscar Lugo <oscaremiliolugo@gmail.com>
2022-06-06 14:41:42 -0400

    update

--- wezterm.lua
+++ wezterm.lua
@@ -1,13 +1,16 @@
 
 local wezterm = require 'wezterm';
+-- local themes = ['Gruvbox Dark',"GitHub Dark","Grape"];
+
 return {
   enable_tab_bar = false,
-  window_background_opacity = 0.8,
-  color_scheme = "Calamity",
+  window_background_opacity = 0.9,
+  color_scheme = "Gruvbox Dark",
   font = wezterm.font("Caskaydia Cove Nerd Font"),
   enable_scroll_bar = true,
+  warn_about_missing_glyphs = false,
   keys = {
     {key="n", mods="CTRL", action="SpawnWindow"},
     {key="l", mods="CTRL|SHIFT", action=wezterm.action{ClearScrollback="ScrollbackOnly"}},
-  }
+  },
 }
