b0b05b4b1121c4c3952eac0f2a872ce6b0efbf2d
racso2609 <oscaremiliolugo@gmail.com>
2022-08-17 11:53:19 -0400

    fix opacity

--- wezterm.lua
+++ wezterm.lua
@@ -1,10 +1,9 @@
-
 local wezterm = require 'wezterm';
 -- local themes = ['Gruvbox Dark',"GitHub Dark","Grape"];
 
 return {
   enable_tab_bar = false,
-  window_background_opacity = 0.9,
+  window_background_opacity = 0.89,
   color_scheme = "Gruvbox Dark",
   font = wezterm.font("Caskaydia Cove Nerd Font"),
   enable_scroll_bar = true,
@@ -12,7 +11,7 @@ return {
   font_size = 13.5,
   -- font_size = 15.5,
   keys = {
-    {key="n", mods="CTRL", action="SpawnWindow"},
-    {key="l", mods="CTRL|SHIFT", action=wezterm.action{ClearScrollback="ScrollbackOnly"}},
+    { key = "n", mods = "CTRL", action = "SpawnWindow" },
+    { key = "l", mods = "CTRL|SHIFT", action = wezterm.action { ClearScrollback = "ScrollbackOnly" } },
   },
 }
