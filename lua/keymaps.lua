7c65571fb954a56d403c851ed42c0973fe81d938
racso2609 <oscaremiliolugo@gmail.com>
2022-09-26 09:36:42 -0400

    update

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,78 +1,82 @@
 local status, wezterm = pcall(require, "wezterm")
-if (not status) then return end
-
+if not status then
+	return
+end
 
 local act = wezterm.action
-local MOVE_SPEED = 5;
+local MOVE_SPEED = 5
 local keymaps = {}
 
-
-
 keymaps.keys = {
-  -- spawn windows and tabs
-  { key = "n", mods = "CTRL", action = "SpawnWindow" },
-  { key = "t", mods = "CTRL", action = act.SpawnTab 'CurrentPaneDomain' },
-  -- split
-  {
-    key = 'S',
-    mods = 'CTRL',
-    action = act.SplitVertical { domain = 'CurrentPaneDomain' }
-  },
-  {
-    key = 'H',
-    mods = 'CTRL',
-    action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }
-  },
-  -- close
-  {
-    key = 'W',
-    mods = 'CTRL',
-    action = act.CloseCurrentPane { confirm = true },
-  },
-  -- toggle full screen
-  {
-    key = 'f',
-    mods = 'ALT',
-    action = act.TogglePaneZoomState,
-  },
-
+	-- spawn windows and tabs
+	{ key = "n", mods = "CTRL", action = "SpawnWindow" },
+	{ key = "t", mods = "CTRL", action = act.SpawnTab("CurrentPaneDomain") },
+	-- split
+	{
+		key = "S",
+		mods = "CTRL",
+		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "H",
+		mods = "CTRL",
+		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	-- close
+	{
+		key = "W",
+		mods = "CTRL",
+		action = act.CloseCurrentPane({ confirm = true }),
+	},
+	-- toggle full screen
+	{
+		key = "f",
+		mods = "ALT",
+		action = act.TogglePaneZoomState,
+	},
 }
 
 local resizeKeys = {
-  { key = 'h', direction = 'Left', space = MOVE_SPEED },
-  { key = 'l', direction = 'Right', space = MOVE_SPEED },
-  { key = 'k', direction = 'Up', space = MOVE_SPEED },
-  { key = 'j', direction = 'Down', space = MOVE_SPEED },
+	{ key = "h", direction = "Left", space = MOVE_SPEED },
+	{ key = "l", direction = "Right", space = MOVE_SPEED },
+	{ key = "k", direction = "Up", space = MOVE_SPEED },
+	{ key = "j", direction = "Down", space = MOVE_SPEED },
+
+	{ key = "LeftArrow", direction = "Left", space = MOVE_SPEED },
+	{ key = "RightArrow", direction = "Right", space = MOVE_SPEED },
+	{ key = "UpArrow", direction = "Up", space = MOVE_SPEED },
+	{ key = "DownArrow", direction = "Down", space = MOVE_SPEED },
 }
 
-
 for i = 1, #resizeKeys do
-  -- CTRL+ALT + number to activate that tab
-  local action = resizeKeys[i];
-  table.insert(keymaps.keys, {
-    key = action.key,
-    mods = 'CTRL|ALT', action = act.AdjustPaneSize { action.direction, action.space },
-  }) -- -act.ActivatePaneDirection- s
+	-- CTRL+ALT + hjk or arrows to resize
+	local action = resizeKeys[i]
+	table.insert(keymaps.keys, {
+		key = action.key,
+		mods = "SHIFT|ALT",
+		action = act.AdjustPaneSize({ action.direction, action.space }),
+	}) -- -act.ActivatePaneDirection- s
 
-  table.insert(keymaps.keys, {
-    key = action.key,
-    mods = 'ALT',
-    action = act.ActivatePaneDirection(action.direction),
-  })
-
+	-- ALT + hjk or arrows tofocus panel
+	table.insert(keymaps.keys, {
+		key = action.key,
+		mods = "ALT",
+		action = act.ActivatePaneDirection(action.direction),
+	})
 end
 for i = 1, 8 do
-  -- CTRL+ALT + number to activate that tab
-  table.insert(keymaps.keys, {
+	-- CTRL+ALT + number to activate that tab
+	table.insert(keymaps.keys, {
 
-    key = tostring(i),
-    mods = 'CTRL|ALT',
-    action = act.ActivateTab(i - 1),
-  })
-  -- F1 through F8 to activate that tab
-  -- table.insert(keymaps.keys, {
-  -- key = 'F' .. tostring(i),
-  -- action = act.ActivateTab(i - 1),
-  -- })
+		key = tostring(i),
+		mods = "CTRL|ALT",
+		action = act.ActivateTab(i - 1),
+	})
+	-- F1 through F8 to activate that tab
+	-- table.insert(keymaps.keys, {
+	-- key = 'F' .. tostring(i),
+	-- action = act.ActivateTab(i - 1),
+	-- })
 end
+
 return keymaps
--- wezterm.lua
+++ wezterm.lua
@@ -1,25 +1,23 @@
-local keys = require('lua.keymaps').keys
-local theme = require('lua.theme')
+local keys = require("lua.keymaps").keys
+local theme = require("lua.theme")
 
 local config = {
-  enable_tab_bar = true,
-  window_background_opacity = 0.75,
-  color_scheme = theme.theme,
-  font = theme.font,
-  enable_scroll_bar = true,
-  warn_about_missing_glyphs = false,
+	enable_tab_bar = true,
+	window_background_opacity = 0.75,
+	color_scheme = theme.theme,
+	font = theme.font,
+	enable_scroll_bar = true,
+	warn_about_missing_glyphs = false,
 
-  -- enable_fancy_tab_bae = false,
-  font_size = 14.8,
-  -- font_size = 15.5,
-  keys = keys,
-  inactive_pane_hsb = {
-    saturation = 0.9,
-    brightness = 0.2,
-  },
-  colors = theme.colors
+	-- enable_fancy_tab_bae = false,
+	font_size = 14.8,
+	-- font_size = 15.5,
+	keys = keys,
+	inactive_pane_hsb = {
+		saturation = 0.9,
+		brightness = 0.2,
+	},
+	colors = theme.colors,
 }
 
-
-
-return config;
+return config
