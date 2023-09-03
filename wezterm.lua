fd1ae7bec013e6cb0c801f9b75f8f18719fc9060
racso2609 <oscaremiliolugo@gmail.com>
2023-09-03 12:59:52 -0400

    hide tabbar

--- wezterm.lua
+++ wezterm.lua
@@ -3,14 +3,15 @@ local theme = require("lua.theme")
 
 local config = {
 	enable_tab_bar = true,
-	window_background_opacity = 0.85,
+	hide_tab_bar_if_only_one_tab = true,
+	-- window_background_opacity = 0.95,
 	color_scheme = theme.theme,
 	font = theme.font,
 	enable_scroll_bar = true,
 	warn_about_missing_glyphs = false,
 
-	-- font_size = 14.8,
-	font_size = 15.5,
+	font_size = 13,
+	-- font_size = 15.5,
 	keys = keys,
 	inactive_pane_hsb = {
 		saturation = 0.9,
